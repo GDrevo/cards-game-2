b35e7d2d165d55174ddefbca31812bb8a9b6bea6
guillaume drevo <guillaume.drevo@gmail.com>
2023-02-27 00:49:42 +0100

    added the index showing the different cards

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,8 @@
+class CardsController < ApplicationController
+  def index
+    player = current_user.player
+    @cards = player.cards.where(unlocked: true).sort_by(&:level).reverse
+    @cards_locked = player.cards.select { |card| card.unlocked == false }
+    @cards_locked = @cards_locked.sort_by(&:shards).reverse
+  end
+end
