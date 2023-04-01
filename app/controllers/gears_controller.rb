6d1fa53c09e5c84c77c0bcb87b039842b0a906a1
guillaume drevo <guillaume.drevo@gmail.com>
2023-04-01 14:14:22 +0200

    added photos attached to gear

--- app/controllers/gears_controller.rb
+++ app/controllers/gears_controller.rb
@@ -1,7 +1,6 @@
 class GearsController < ApplicationController
   def index
     params[:gear_type] && gear_type = params[:gear_type]
-    @gears = current_user.player.gears
-    gear_type && @gears = @gears.where(gear_type:)
+    gear_type && @gears = current_user.player.gears.where(gear_type:)
   end
 end
--- app/models/gear.rb
+++ app/models/gear.rb
@@ -1,3 +1,34 @@
 class Gear < ApplicationRecord
   belongs_to :gear_set
+  has_one_attached :photo
+
+  after_create :attach_photo
+
+  private
+
+  def attach_photo
+    return if photo.attached?
+
+    case gear_type
+    when "weapon 0"
+      photo.attach(io: File.open('app/assets/images/weapon-gear 0.png'), filename: 'weapon-gear 0.png', content_type: "image/png")
+    when "weapon 1"
+      photo.attach(io: File.open('app/assets/images/weapon-gear 1.png'), filename: 'weapon-gear 1.png', content_type: "image/png")
+    when "weapon 2"
+      photo.attach(io: File.open('app/assets/images/weapon-gear 2.png'), filename: 'weapon-gear 2.png', content_type: "image/png")
+    when "armor 0"
+      photo.attach(io: File.open('app/assets/images/armor-gear 0.png'), filename: 'armor-gear 0.png', content_type: "image/png")
+    when "armor 1"
+      photo.attach(io: File.open('app/assets/images/armor-gear 1.png'), filename: 'armor-gear 1.png', content_type: "image/png")
+    when "armor 2"
+      photo.attach(io: File.open('app/assets/images/armor-gear 2.png'), filename: 'armor-gear 2.png', content_type: "image/png")
+    when "artifact 0"
+      photo.attach(io: File.open('app/assets/images/artifact-gear 0.png'), filename: 'artifact-gear 0.png', content_type: "image/png")
+    when "artifact 1"
+      photo.attach(io: File.open('app/assets/images/artifact-gear 1.png'), filename: 'artifact-gear 1.png', content_type: "image/png")
+    when "artifact 2"
+      photo.attach(io: File.open('app/assets/images/artifact-gear 2.png'), filename: 'artifact-gear 2.png', content_type: "image/png")
+    end
+    save
+  end
 end
