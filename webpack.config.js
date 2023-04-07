51c7afd73d6ee139fdec2bbca5b956973732f099
guillaume drevo <guillaume.drevo@gmail.com>
2023-04-07 12:05:11 +0200

    initial setup for react front done

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,20 @@ module.exports = {
     sourceMapFilename: "[file].map",
     path: path.resolve(__dirname, "app/assets/builds"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
       maxChunks: 1
